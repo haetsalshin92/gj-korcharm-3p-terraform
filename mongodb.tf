faaae12581b3b56ce697eee439d74bca6ec4e0fe
haetsalshin92 <sinsin09022@gmail.com>
2025-07-24 09:28:10 +0900

    mongoDB 키 추가

--- mongodb.tf
+++ mongodb.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "mongodb" {
   ami                         = "ami-0e770019ca3d79961" 
   instance_type               = "t2.micro"
+  key_name                    = "gj-test2"
   subnet_id                   = aws_subnet.public_1a_mongo.id
   vpc_security_group_ids      = [aws_security_group.sg_mongodb.id]
   associate_public_ip_address = false 
